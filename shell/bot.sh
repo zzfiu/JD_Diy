66c0dcab3105478e104488ee6e1227ce7f1f25c2
张子枫 <1048411925@qq.com>
2022-04-09 13:11:56 +0800

    Update bot.sh

--- shell/bot.sh
+++ shell/bot.sh
@@ -10,7 +10,7 @@ file_botset=$root/config/botset.json
 file_diybotset=$root/config/diybotset.json
 
 branch="main"
-url="https://github.com/chiupam/JD_Diy.git"
+url="https://github.com/zzfiu/JD_Diy.git"
 
 main() {
   echo -e "\n$1\n"
